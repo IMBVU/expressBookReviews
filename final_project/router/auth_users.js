0aed59e7657408e9c7765c4563e1304dad50cb11
Bryan Vu <134100395+IMBVU@users.noreply.github.com>
2024-08-02 00:28:12 -0400

    book review auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -35,8 +35,21 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, jwtSecret);
+    const username = decoded.username;
+
+    if (books[isbn]) {
+        if (!books[isbn].reviews) {
+            books[isbn].reviews = {};
+        }
+        books[isbn].reviews[username] = review;
+        res.status(200).json({ message: "Review added/updated successfully" });
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.authenticated = regd_users;
